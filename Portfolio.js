5769b3d2028fa488cc73060a475e476da5b46f48
yuvrajsinhsolanki1019 <yuvrajsinhsolanki6680@gmail.com>
2025-07-01 14:57:35 +0530

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,9 @@
+// Example: Smooth scroll to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
